8f14d7f2dc9c825e61827a0d849332adf44dd89b
Ko <stefan.j.ko@accenture.com>
2019-06-11 15:39:55 -0400

    added ASGI

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+ASGI_APPLICATION = 'mysite.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
