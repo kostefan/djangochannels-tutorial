0bb003a5aa2fa2bb9e972136ca6ad7a2904cb51d
kostefan <kostefan@github.com>
2019-06-18 14:36:02 -0400

    changed urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url
-from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('chat', include('chat.urls')),
-    path('admin', admin.site.urls),
+    url(r'^chat/', include('chat.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
